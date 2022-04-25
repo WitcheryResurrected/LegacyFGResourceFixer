d0794741fe71ee13783f4dce3ba0afbdbf45d4e3
Ashley Wright <ashley@msrandom.net>
2022-04-25 19:17:34 +0300

    Move directory merging strategy out, opting to instead allow it to be handled via env variables

--- src/main/java/net/msrandom/resourcefixer/ResourceFixerTweaker.java
+++ src/main/java/net/msrandom/resourcefixer/ResourceFixerTweaker.java
@@ -9,18 +9,12 @@ import java.lang.reflect.Field;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
 
 public class ResourceFixerTweaker implements ITweaker {
-    public static final CompletableFuture<?>[] COMPLETABLE_FUTURES_DUMMY = new CompletableFuture<?>[0];
-
     @Override
     public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
     }
@@ -30,20 +24,18 @@ public class ResourceFixerTweaker implements ITweaker {
         return field.get(receiver);
     }
 
-    private static Path getCommonPath(List<Path> paths) {
-        Path commonDirectory = paths.get(0);
-        while (true) {
-            boolean done = true;
-            for (Path directory : paths) {
-                if (!directory.startsWith(commonDirectory)) {
-                    done = false;
-                    break;
+    private static void handleEnv(String env, BiConsumer<String, Path> callback) {
+        String modClasses = System.getenv("MOD_CLASSES");
+        if (modClasses != null) {
+            for (String output : modClasses.split(":")) {
+                String[] paths = output.split("%%");
+                if (paths.length != 0) {
+                    String id = paths.length > 1 ? paths[0] : null;
+                    Path file = Paths.get(paths[paths.length - 1]);
+                    callback.accept(id, file);
                 }
             }
-            if (done) break;
-            commonDirectory = commonDirectory.getParent();
         }
-        return commonDirectory;
     }
 
     @Override
@@ -54,20 +46,17 @@ public class ResourceFixerTweaker implements ITweaker {
             @SuppressWarnings("unchecked")
             List<URL> path = (List<URL>) get(ucp, ucp.getClass().getDeclaredField("path"));
 
+            Map<String, Path> mergedOutputs = new HashMap<>();
+            handleEnv("MERGED_OUTPUT", mergedOutputs::put);
+
             Map<String, List<Path>> classpathFiles = new HashMap<>();
             Map<Path, String> fileToId = new HashMap<>();
-            String modClasses = System.getenv("MOD_CLASSES");
-            if (modClasses != null) {
-                for (String output : modClasses.split(":")) {
-                    String[] paths = output.split("%%");
-                    if (paths.length != 0) {
-                        String id = paths.length > 1 ? paths[0] : null;
-                        Path file = Paths.get(paths[paths.length - 1]);
-                        classpathFiles.computeIfAbsent(id, key -> new ArrayList<>()).add(file);
-                        fileToId.put(file, id);
-                    }
+            handleEnv("MOD_CLASSES", (id, file) -> {
+                if (mergedOutputs.containsKey(id)) {
+                    classpathFiles.computeIfAbsent(id, key -> new ArrayList<>()).add(file);
+                    fileToId.put(file, id);
                 }
-            }
+            });
 
             Set<String> processedIds = new HashSet<>();
             Set<String> failedIds = new HashSet<>();
@@ -94,36 +83,10 @@ public class ResourceFixerTweaker implements ITweaker {
 
                     if (processedIds.contains(id)) continue;
 
-                    Path redirectedOutput = getCommonPath(paths).resolve(id == null ? "redirectedOutput" : id + "RedirectedOutput");
+                    Path mergedOutput = mergedOutputs.get(id);
                     try {
-                        List<CompletableFuture<Void>> futures = new ArrayList<>();
-                        for (Path directory : paths) {
-                            if (Files.exists(directory)) {
-                                Files.walk(directory).filter(Files::isRegularFile).map(file -> CompletableFuture.runAsync(() -> {
-                                    try {
-                                        MessageDigest md5 = MessageDigest.getInstance("MD5");
-                                        Path newLocation = redirectedOutput.resolve(directory.relativize(file));
-                                        if (Files.exists(newLocation)) {
-                                            md5.update(Files.readAllBytes(file));
-                                            byte[] fileMd5 = md5.digest();
-                                            md5.update(Files.readAllBytes(newLocation));
-                                            if (!Arrays.equals(fileMd5, md5.digest())) {
-                                                Files.copy(file, newLocation, StandardCopyOption.REPLACE_EXISTING);
-                                            }
-                                        } else {
-                                            Files.createDirectories(newLocation.getParent());
-                                            Files.copy(file, newLocation);
-                                        }
-                                    } catch (IOException | NoSuchAlgorithmException e) {
-                                        throw new RuntimeException(e); // throw outside the lambda
-                                    }
-                                })).forEach(futures::add);
-                            }
-                        }
-
-                        CompletableFuture.allOf(futures.toArray(COMPLETABLE_FUTURES_DUMMY)).join();
                         processedIds.add(id);
-                        fixedPath.add(redirectedOutput.toUri().toURL());
+                        fixedPath.add(mergedOutput.toUri().toURL());
                     } catch (IOException | RuntimeException e) {
                         failedIds.add(id);
                         fixedPath.add(url);
